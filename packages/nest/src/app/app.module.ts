77ebb4f968d01a4fc59bbdcee0d45791ec84815e
1ike <googlohren@gmail.com>
2023-01-06 17:37:26 +0300

    Add static serving to nest.

--- packages/nest/src/app/app.module.ts
+++ packages/nest/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppGateway } from './app.gateway';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'packages', 'react'),
+    }),
+  ],
   providers: [AppService, AppGateway],
 })
 export class AppModule {}
