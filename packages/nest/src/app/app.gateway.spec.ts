93de52e1787307691ccdf45cee50fb085eba6923
1ike <googlohren@gmail.com>
2023-01-17 20:29:55 +0300

    Fix the tests that are left over from the scaffolding.

--- packages/nest/src/app/app.gateway.spec.ts
+++ packages/nest/src/app/app.gateway.spec.ts
@@ -1,12 +1,22 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppGateway } from './app.gateway';
+import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 describe('AppGateway', () => {
   let gateway: AppGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AppGateway],
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+          isGlobal: true,
+        }),
+      ],
+      providers: [AppService, AppGateway],
     }).compile();
 
     gateway = module.get<AppGateway>(AppGateway);
--- packages/react/src/pages/tender/tender.spec.tsx
+++ packages/react/src/pages/tender/tender.spec.tsx
@@ -1,15 +1,16 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import Tender from '.';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Tender />);
+    const { baseElement } = render(<Tender />, { wrapper: MemoryRouter });
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<Tender />);
+  it('should have a main header', () => {
+    const { getByText } = render(<Tender />, { wrapper: MemoryRouter });
     expect(getByText(/Тестовые торги/gi)).toBeTruthy();
   });
 });
