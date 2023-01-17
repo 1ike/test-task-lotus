5cb0d10a6691daa55a4f5d10f817a951eb0f4150
1ike <googlohren@gmail.com>
2023-01-17 17:42:58 +0300

    Fix of default room deletion prevention.

--- packages/react/src/config.ts
+++ packages/react/src/config.ts
@@ -2,7 +2,7 @@ export const SERVER_URL = process.env.NX_SERVER_URL?.trim() || 'http://localhost
 export const SERVER_PORT = parseInt(process.env.NX_SERVER_PORT || '', 10) || 3333;
 export const API_PREFIX = process.env.NX_API_PREFIX?.trim() || 'api';
 
-export const DEFAULT_ROOM_NAME = process.env.NX_ROOM_NAME || 123;
+export const DEFAULT_ROOM_NAME = process.env.NX_ROOM_NAME || '123';
 
 export const TIMER = process.env.NX_TIMER || '120';
 
